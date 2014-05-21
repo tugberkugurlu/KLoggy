984209bbb649904ba3a1acdf5a4090c837ab64b1
tugberkugurlu <tugberkugurlu@gmail.com>
2014-05-21 23:16:54 +0300

    added localconfig.json, added a few more dependencies

--- src/KLoggy.Web/Startup.cs
+++ src/KLoggy.Web/Startup.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNet.Builder;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
 using System;
 
 namespace KLoggy.Web 
 {
     public class Startup 
     {
-        public void Configure(IBuilder builder)
+        public void Configure(IBuilder app)
         {
+            app.UseServices(services =>
+            {
+            });
+            
             Console.WriteLine(string.Concat("Hello from '", typeof(Startup).FullName, "'"));
         }
     }
