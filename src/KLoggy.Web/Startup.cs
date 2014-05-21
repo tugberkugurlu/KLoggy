6ac01b43f958dbaebfe643fa1fc1825862d20d03
tugberkugurlu <tugberkugurlu@gmail.com>
2014-05-21 23:55:14 +0300

    added configuration instances and outputed them to the screen

--- src/KLoggy.Web/Startup.cs
+++ src/KLoggy.Web/Startup.cs
@@ -11,6 +11,18 @@ namespace KLoggy.Web
         {
             app.UseServices(services =>
             {
+                Configuration configuration = new Configuration();
+                configuration.AddJsonFile("LocalConfig.json");
+                configuration.AddEnvironmentVariables();
+                services.AddInstance<IConfiguration>(configuration);
+                foreach(var source in configuration)
+                {
+                    Console.WriteLine("==={0}===", ((BaseConfigurationSource)source).GetType());
+                    foreach(var kvp in ((BaseConfigurationSource)source).Data)
+                    {
+                        Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+                    }
+                }
             });
             
             Console.WriteLine(string.Concat("Hello from '", typeof(Startup).FullName, "'"));
