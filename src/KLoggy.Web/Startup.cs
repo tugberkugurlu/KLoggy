024390a30630a641f9d86d0c47e420c3d75d67cf
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-01 15:54:28 +0300

    updated according to aspnetvnext feed. Still not working though.

--- src/KLoggy.Web/Startup.cs
+++ src/KLoggy.Web/Startup.cs
@@ -1,22 +1,11 @@
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Routing;
-using Microsoft.Framework.DependencyInjection;
 
 namespace KLoggy.Web 
 {
     public class Startup 
     {
-        public void Configure(IBuilder app)
+        public void Configure(IApplicationBuilder app)
         {
-            app.UseFileServer();
-            app.UseErrorPage();
-
-            app.UseServices(services =>
-            {
-                services.AddMvc();
-            });
-
-            app.UseMvc();
         }
     }
 }
