f2c262a2c9f578d7d407d78fd648c34df8d75c8e
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-02 13:11:21 +0300

    listing the configs inside the startup

--- src/KLoggy.Web/Startup.cs
+++ src/KLoggy.Web/Startup.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNet.Builder;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using System;
 
 namespace KLoggy.Web 
 {
@@ -6,6 +9,21 @@ namespace KLoggy.Web
     {
         public void Configure(IApplicationBuilder app)
         {
+            app.UseServices(services =>
+            {
+                Configuration configuration = new Configuration();
+                configuration.AddIniFile("git.ini");
+                foreach(var source in configuration)
+                {
+                    Console.WriteLine("==={0}===", ((BaseConfigurationSource)source).GetType());
+                    foreach(var kvp in ((BaseConfigurationSource)source).Data)
+                    {
+                        Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+                    }
+                }
+            });
+            
+            Console.WriteLine(string.Concat("Hello from '", typeof(Startup).FullName, "'"));
         }
     }
 }
