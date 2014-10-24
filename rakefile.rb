e47cc4908394c6332727dee91166025a1fa1ff05
tugberkugurlu <tugberkugurlu@gmail.com>
2014-10-24 20:56:02 +0300

    added npm, bower and gulp tasks to rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,4 @@
+require 'os'
 require 'rake'
 require 'rake/clean'
 
@@ -11,17 +12,51 @@ ARTIFACTSAPPSROOT = File.join(ARTIFACTSROOT, "apps")
 PROJECTFILENAME = "project.json"
 CONFIGURATION = 'Release'
 
+APPPROJECTFILES = Dir[File.join(SRCROOT, "**", PROJECTFILENAME)]
+TESTROJECTFILES = Dir[File.join(TESTROOT, "**", PROJECTFILENAME)]
+
 CLEAN.include(ARTIFACTSROOT)
 CLEAN.include(File.join(TESTROOT, "**", "**", "bin"))
 CLEAN.include(File.join(SRCROOT, "**", "**", "bin"))
 CLEAN.include(File.join(SRCROOT, "**", "**", "node_modules"))
 CLEAN.include(File.join(SRCROOT, "**", "**", "bower_components"))
 
-task:default => [:clean, :check] do
-end
+task:default => [:clean, :check, :npm, :bower, :gulp]
 
 task:check do
     %w(npm bower kpm gulp).each do |cmd|
-        begin sh "#{cmd} --version > NUL" rescue raise "#{cmd} doesn't exists globally" end
+        begin sh "#{cmd} --version >> #{OS.dev_null}" rescue raise "#{cmd} doesn't exists globally" end
+    end
+end
+
+task:npm do
+    APPPROJECTFILES.each do |file|
+        _projectDir = File.dirname(file)
+        _packageFilePath = File.join(_projectDir, "package.json")
+        if File.exists?(_packageFilePath)
+            sh "npm install --prefix \"#{_projectDir}\""
+        end
+    end
+end
+
+task:bower do
+    APPPROJECTFILES.each do |file|
+        _projectDir = File.dirname(file)
+        _bowerFilePath = File.join(_projectDir, "bower.json")
+        if File.exists?(_bowerFilePath)
+            # TODO: rely on .bowerrc file for now. config.cwd is so weird. ref: https://github.com/bower/bower/issues/1561
+            # sh "bower install --config.cwd \"#{_bowerFilePath}\""
+            sh "bower install"
+        end
+    end
+end
+
+task:gulp do
+    APPPROJECTFILES.each do |file|
+        _projectDir = File.dirname(file)
+        _gulpFilePath = File.join(_projectDir, "gulpfile.js")
+        if File.exists?(_gulpFilePath)
+            sh "gulp --cwd \"#{_projectDir}\""
+        end
     end
 end
